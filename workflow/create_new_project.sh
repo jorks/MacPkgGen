e410433a7797846c4344834c9c7e40a8c8f67f0e
James Corcoran <jorks@users.noreply.github.com>
2023-03-15 16:20:48 +1100

    New Package Script

--- workflow/create_new_project.sh
+++ workflow/create_new_project.sh
@@ -1,5 +1,19 @@
-#!/bin/sh
-
+#!/bin/bash
+# set -x
 # This script can be used to spawn your own repo.
 
 # Will need to set the exicutble bit on scripts in the workflows dir
+
+NEW_PACKAGE_NAME="my_new_package2"
+NEW_PACKAGE_REMOTE="https://github.com/jorks/my_new_package2.git"
+
+JORKS_TEMPLATE_REPO="https://github.com/jorks/Jamf-Prestage-Assets.git"
+
+git clone "${JORKS_TEMPLATE_REPO}" "${NEW_PACKAGE_NAME}" && cd "${NEW_PACKAGE_NAME}" || exit
+rm -rf .git
+git init
+git add .
+git commit -m "New package created: ${NEW_PACKAGE_NAME}"
+git remote add origin "${NEW_PACKAGE_REMOTE}"
+git branch -M main
+git push -u origin main
